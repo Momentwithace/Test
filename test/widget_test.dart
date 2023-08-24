5d7d9c0ff3ef3b53ddde73c533b0ecec3d6ea144
momemtwithace <augustineezekiel763@gmail.com>
2023-08-24 16:24:29 +0100

    refactored

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,60 +1,59 @@
-// import 'package:flutter/material.dart';
-// import 'package:flutter_test/flutter_test.dart';
-// import 'package:test/main.dart'; // Import CardData class
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:test/main.dart';
 
-// void main() {
-//   test('Toggle favorite state', () {
-//     final cardData = CardData(
-//       imageUrl: 'https://via.placeholder.com/150',
-//       title: 'Test Card',
-//       description: 'Test Description',
-//     );
+void main() {
+  test('Toggle favorite state', () {
+    final cardData = CardData(
+      imageUrl: 'https://via.placeholder.com/150',
+      title: 'Test Card',
+      description: 'Test Description',
+    );
 
-//     expect(cardData.isFavorite, false);
+    expect(cardData.isFavorite, false);
 
-//     cardData.isFavorite = true;
-//     expect(cardData.isFavorite, true);
+    cardData.isFavorite = true;
+    expect(cardData.isFavorite, true);
 
-//     cardData.isFavorite = false;
-//     expect(cardData.isFavorite, false);
-//   });
+    cardData.isFavorite = false;
+    expect(cardData.isFavorite, false);
+  });
 
-//   testWidgets('List items are displayed correctly', (WidgetTester tester) async {
-//     final List<CardData> dummyData = [
-//       CardData(
-//         imageUrl: 'https://via.placeholder.com/150',
-//         title: 'Card 1',
-//         description: 'Description for Card 1',
-//       ),
-//       // Add more dummy data here
-//     ];
+  testWidgets('List items are displayed correctly', (WidgetTester tester) async {
+    final List<CardData> dummyData = [
+      CardData(
+        imageUrl: 'https://via.placeholder.com/150',
+        title: 'Card 1',
+        description: 'Description for Card 1',
+      ),
+      // Add more dummy data here
+    ];
 
-//     await tester.pumpWidget(MaterialApp(home: CardList(cards: "de", isFavorite: false,)));
+    await tester.pumpWidget(MaterialApp(home: CardList()));
 
-//     // Verify if the list items are displayed correctly
-//     expect(find.text('Card 1'), findsOneWidget);
-//     // Add more assertions for other list items
-//   });
+    // Verify if the list items are displayed correctly
+    expect(find.text('Card 1'), findsOneWidget);
+    // Add more assertions for other list items
+  });
 
-//   testWidgets('Navigate from list to detail screen', (WidgetTester tester) async {
-//     final List<CardData> dummyData = [
-//       CardData(
-//         imageUrl: 'https://via.placeholder.com/150',
-//         title: 'Card 1',
-//         description: 'Description for Card 1',
-//       ),
-//       // Add more dummy data here
-//     ];
+  testWidgets('Navigate from list to detail screen', (WidgetTester tester) async {
+    final List<CardData> dummyData = [
+      CardData(
+        imageUrl: 'https://via.placeholder.com/150',
+        title: 'Card 1',
+        description: 'Description for Card 1',
+      ),
+      // Add more dummy data here
+    ];
 
-//     await tester.pumpWidget(MaterialApp(home: CardList(cards: dummyData)));
+    await tester.pumpWidget(MaterialApp(home: CardList()));
 
-//     // Tap on the list item
-//     await tester.tap(find.text('Card 1'));
-//     await tester.pumpAndSettle();
+    // Tap on the list item
+    await tester.tap(find.text('Card 1'));
+    await tester.pumpAndSettle();
 
-//     // Verify if the detail screen is displayed
-//     expect(find.text('Detail Screen'), findsOneWidget);
-//     // Add more assertions for the detail screen content
-//   });
-// }
-
+    // Verify if the detail screen is displayed
+    expect(find.text('Detail Screen'), findsOneWidget);
+    // Add more assertions for the detail screen content
+  });
+}
